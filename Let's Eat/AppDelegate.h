35a8162cd0407c4ac5537b8a210f6f38f78a4c2c
Ryan Kass <kassr30@gmail.com>
2014-02-10 13:23:50 -0500

    Initial Commit

---
+++ Let's Eat/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Let's Eat
+//
+//  Created by Ryan Kass on 2/10/14.
+//  Copyright (c) 2014 Ryan Kass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Let's Eat/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Let's Eat
+//
+//  Created by Ryan Kass on 2/10/14.
+//  Copyright (c) 2014 Ryan Kass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
